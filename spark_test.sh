ac9298efcdcc9074dce5badef4944c11bc7ced34
Eaton, Bryan <eatonbm@ornl.gov>
2021-04-07 11:49:44 -0400

    wanting twitter credentials

--- spark_test.sh
+++ spark_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+# Use with Spark 2.4.6 in cluster and local
 spark-submit \
     --master 'local[*]' \
     --conf spark.ui.port=8082 \
@@ -7,8 +9,5 @@ spark-submit \
     --conf spark.executor.memoryOverhead=16g \
     --conf spark.driver.maxResultSize=16g \
     --class edu.umd.cs.hcil.twitter.spark.stream.App \
-    target/BurstyTwitterStream-2.0-jar-with-dependencies.jar \
-    ./conf/streamer.properties \
-    ./conf/topics.json \
-    log_file.log \
-    ./conf/filter_keywords.txt
+    --jars target/BurstyTwitterStream-2.0-jar-with-dependencies.jar \
+    target/BurstyTwitterStream-2.0-jar-with-dependencies.jar ./conf/streamer.properties ./conf/topics.json log_file.log ./conf/filter_keywords.txt
