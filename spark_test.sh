609e43a673c4371063016fa2cb92ca288ff198dd
Eaton, Bryan <eatonbm@ornl.gov>
2021-04-23 11:38:18 -0400

    update example script, compose

--- spark_test.sh
+++ spark_test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 
+
+
+
 # Use with Spark 2.4.6 in cluster and local
 spark-submit \
-    --master 'local[*]' \
-    --conf spark.ui.port=8082 \
+    --master spark://pscore:7077 \
+    --conf spark.ui.port=9999 \
     --executor-memory 2g \
     --conf spark.executor.memoryOverhead=16g \
     --conf spark.driver.maxResultSize=16g \
     --class edu.umd.cs.hcil.twitter.spark.stream.App \
-    --jars target/BurstyTwitterStream-2.0-jar-with-dependencies.jar \
     target/BurstyTwitterStream-2.0-jar-with-dependencies.jar ./conf/streamer.properties ./conf/topics.json log_file.log ./conf/filter_keywords.txt
